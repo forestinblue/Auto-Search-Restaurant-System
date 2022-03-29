bdd9dd42a328f8db467d757956bb140d688718d1
Juun <90318043+forestinblue@users.noreply.github.com>
2022-03-29 17:54:40 +0900

    change class to function

--- NewRestaurantDataCollect/NewRestaurantUploadFlask.py
+++ NewRestaurantDataCollect/NewRestaurantUploadFlask.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
-import pandas as pd 
-class uploadFlask:
-    global foodsafetykorea_new_information
-    foodsafetykorea_new_information =pd.read_csv('C:/Users/junseok/Downloads/foodsafetykorea_new_information.csv')
-    global app
-    app =  Flask(__name__)
-    app.config['JSON_AS_ASCII'] = False
-    @app.route("/")
-    
-    def upload_json():
-        return jsonify(foodsafetykorea_new_information.to_dict('records'))
+from flask import Flask, jsonify , current_app
+import pandas as pd
 
+def run_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def upload_json():
+        foodsafetykorea_new_information = pd.read_csv(
+            'C:/Users/junseok/Downloads/foodsafetykorea_new_information.csv'
+        )
+        
+        app =  Flask(__name__)
+        app.config['JSON_AS_ASCII'] = False
+        
+        with app.app_context():
+            print(app.app_context)
+         
+        return jsonify(foodsafetykorea_new_information.to_dict('records'))
+        #return foodsafetykorea_new_information.to_dict('records')
+
+    with app.app_context():
+        print (current_app.name)
+
+    app.config['JSON_AS_ASCII'] = False
+    app.run()
+
+if __name__ == "__main__":
+    run_app()
