7585429a6a6c51fd29d7cce735e79ecdb4d62747
Juun <90318043+forestinblue@users.noreply.github.com>
2022-04-01 10:55:43 +0900

    change module, variable name

--- NewRestaurantDataCollect/main.py
+++ NewRestaurantDataCollect/main.py
@@ -1,15 +1,9 @@
 # python main.pymain.py
-from NewRestaurantAutoCollect import GatherNewRestaurantData
-from NewRestaurantUploadFlask import run_app
+from new_restaurant_auto_collect import GatherNewRestaurantData
+from new_restaurant_upload_flask import run_app
 
 
 if __name__ == '__main__':
     GatherNewRestaurantData.click_next_page_upload_csv()
     GatherNewRestaurantData.crawling_lat_lon_data()
     run_app()
-    
-    
-    
-    
-    
-    
