f543021dc69331c11e250290ca4f834a420fc0ed
Juun <90318043+forestinblue@users.noreply.github.com>
2022-04-06 17:50:23 +0900

    add thread module

--- NewRestaurantDataCollect/main.py
+++ NewRestaurantDataCollect/main.py
@@ -1,9 +1,28 @@
-# python main.pymain.py
-from new_restaurant_auto_collect import GatherNewRestaurantData
 from new_restaurant_upload_flask import run_app
+from new_restaurant_auto_collect import GatherNewRestaurantData
+from threading import Thread
+import time
+import schedule
+
+data = {}
 
 
-if __name__ == '__main__':
-    GatherNewRestaurantData.click_next_page_upload_csv()
-    GatherNewRestaurantData.crawling_lat_lon_data()
-    run_app()
+def crawl():
+    print('im crawling')
+    data.update(GatherNewRestaurantData.crawling_lat_lon_data())
+
+
+schedule.every().wednesday.at("17:39").do(crawl)  # 월요일 00:10분에 실행
+
+
+def crawl_task():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+crawl_thread = Thread(target=crawl_task)
+crawl_thread.start()
+
+run_app(data)
+crawl_thread.join()
--- NewRestaurantDataCollect/new_restaurant_upload_flask.py
+++ NewRestaurantDataCollect/new_restaurant_upload_flask.py
@@ -1,24 +1,18 @@
 from flask import Flask, jsonify, current_app
-import pandas as pd
 
 
-def run_app():
+def run_app(data):
     app = Flask(__name__)
 
     @app.route("/")
     def upload_json():
-        foodsafetykorea_new_restaurant_information = pd.read_csv(
-            'C:/Users/junseok/Downloads/foodsafetykorea_new_restaurant_information.csv'
-        )
-
         app = Flask(__name__)
         app.config['JSON_AS_ASCII'] = False
 
         with app.app_context():
             print(app.app_context)
 
-        return jsonify(foodsafetykorea_new_restaurant_information.to_dict('records'))
-        # return foodsafetykorea_new_information.to_dict('records')
+        return jsonify(data)
 
     with app.app_context():
         print(current_app.name)
